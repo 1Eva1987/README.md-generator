1c35bfb68006307dd60ef9727581ed01642c153c
1Eva1987 <115692772+1Eva1987@users.noreply.github.com>
2023-02-17 15:38:33 +0000

    function writeToFileusing file system module crates needed file using provided data, function init generateMarkdown funtion with argument as responses object passed in and assigned to answers variable then writeToFile funcion called with needed arguments passed in to create readmefile

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# README.md-generator
+# Genetator
+
--- index.js
+++ index.js
@@ -74,12 +74,20 @@ const questions = [
 ];
 
 // function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, (err) =>
+    err ? console.log(err) : console.log("success!!!")
+  );
+}
 
 // function to initialize program
 function init() {
   inquirer.prompt(questions).then((responses) => {
-    console.log("recieved");
+    console.log(responses);
+    const answers = generateMarkdown(responses);
+    writeToFile("readme.md", answers, (err) =>
+      err ? console.log(err) : console.log("made it")
+    );
   });
 }
 
